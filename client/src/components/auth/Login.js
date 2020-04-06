85646247dc7b19bc4a7641950712cf3e895c6e73
guru.bagawan <guru.bagawan@gmail.com>
2020-04-06 13:07:16 -0400

    redux store setup

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('success');
+  };
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into your account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            value={email}
+            required
+            onChange={e => onChange(e)}
+            placeholder="Email Address"
+            name="email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => onChange(e)}
+            name="password"
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
